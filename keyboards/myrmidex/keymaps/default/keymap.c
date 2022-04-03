f977ef0d594917b3ee0ea1998ad6f03d63c51e36
Konstantin <hexagramg@gmail.com>
2022-04-04 00:01:33 +0300

    added mac layer support and changed rotary function accordingly

--- keyboards/myrmidex/keymaps/default/keymap.c
+++ keyboards/myrmidex/keymaps/default/keymap.c
@@ -5,6 +5,7 @@
 
 enum layers {
     _QWERTY = 0,
+    _MAC_QWERTY,
     _NAV,
     _SYM,
     _GAMING,
@@ -14,6 +15,7 @@ enum layers {
 // Aliases for readability
 #define QWERTY DF(_QWERTY)
 #define GAMING DF(_GAMING)
+#define MAC DF(_MAC_QWERTY)
 
 #define SYM MO(_SYM)
 #define NAV MO(_NAV)
@@ -39,19 +41,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *        ,-------------------------------------------.                                                ,-------------------------------------------.
  *        |  Tab   |   Q  |   W  |   E  |   R  |   T  |                                                |   Y  |   U  |   I  |   O  |   P  |  LBRC  |
  *,-------+--------+------+------+------+------+------|                                                |------+------+------+------+------+--------+-------.
- *| GRAVE |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |                                                |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "| RBRC  |
+ *|  ESC  | Ctrl |   A  |   S  |   D  |   F  |   G  |                                                |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "| RBRC  |
  *`-------+--------+------+------+------+------+------|                                                |------+------+------+------+------+--------+-------'
  *        | Enter  |   Z  |   X  |   C  |   V  |   B  |                                                |   N  |   M  | ,  < | . >  | /  ? |  Bksp  |
  *        `----------------------+------+------+------+--|-----------------.      .-----------------|--+------+------+------+----------------------'
- *                               | Menu | LALT | NAV  | Shift| Space| Nav  |      | Space| Sym  | Space|  SYM | RGUI |GAMING|                                 
+ *                               | Menu | LALT | NAV  | Shift| Space| LCTL |      | RGUI | Sym  | Space|  SYM | RGUI |GAMING|                                 
  *                               |      |      |      |      |      |      |      |      |      |      |      |      |      |                                 
  *                               `-----------------------------------------'      `-----------------------------------------'                                 
  */
     [_QWERTY] = LAYOUT(\
               KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_LBRC, \
-     KC_GRV , CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT, KC_RBRC, \
+     KC_ESC , KC_LCTL , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT, KC_RBRC, \
               KC_ENT  , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B ,                                        KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_BSPC, \
-                                KC_APP , KC_LALT,   NAV  , KC_LSFT,KC_LCTL,KC_NO,     KC_NO,KC_LCTL, KC_SPC,   SYM  ,KC_RGUI, GAMING),
+                                KC_APP , KC_LALT,   NAV  , KC_LSFT,KC_LCTL,KC_NO,     KC_NO,KC_RGUI, KC_SPC,   SYM  ,KC_RGUI, GAMING),
+/*
+ * Base Layer: MAC QWERTY Layer 
+*
+ *        ,-------------------------------------------.                                                ,-------------------------------------------.
+ *        |  Tab   |   Q  |   W  |   E  |   R  |   T  |                                                |   Y  |   U  |   I  |   O  |   P  |  LBRC  |
+ *,-------+--------+------+------+------+------+------|                                                |------+------+------+------+------+--------+-------.
+ *|GRAVE_ESC| Ctrl |   A  |   S  |   D  |   F  |   G  |                                                |   H  |   J  |   K  |   L  | ;  : |Ctrl/' "| RBRC  |
+ *`-------+--------+------+------+------+------+------|                                                |------+------+------+------+------+--------+-------'
+ *        | Enter  |   Z  |   X  |   C  |   V  |   B  |                                                |   N  |   M  | ,  < | . >  | /  ? |  Bksp  |
+ *        `----------------------+------+------+------+--|-----------------.      .-----------------|--+------+------+------+----------------------'
+ *                               | Menu | LALT | NAV  | Shift| RGUI |      |      |      | LCTL | Space|  SYM | RGUI |GAMING|                                 
+ *                               |      |      |      |      |      |      |      |      |      |      |      |      |      |                                 
+ *                               `-----------------------------------------'      `-----------------------------------------'                                 
+ */
+    [_MAC_QWERTY] = LAYOUT(\
+              KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_LBRC, \
+     KC_GESC ,KC_LCTL , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT, KC_RBRC, \
+              KC_ENT  , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B ,                                        KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_BSPC, \
+                                KC_APP , KC_LALT,   NAV  , KC_LSFT,KC_RGUI,KC_NO,       KC_M,KC_LCTL, KC_SPC,   SYM  ,KC_RGUI , GAMING),
 /*
 
  * MO Layer: NAV This layer is for numbers, arrows and F keys
@@ -63,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *`-------+--------+------+------+------+------+------|                                                |------+------+------+------+------+--------+-------'
  *        |KC_TRNS_| XXXX |   1  |   2  |   3  | XXXX |                                                |  F7  |  F8  | F9   | F10  | F11  |KC_TRNS_|
  *        `----------------------+------+------+------+--|-----------------.      .-----------------|--+------+------+------+----------------------'
- *                               |______|______|______|______|______|______|      |______|______|______|______|______|______|
+ *                               |______|______|______|______|______|______|      |______|______|______|______|______| MAC  |
  *                               |      |      |      |      |      |      |      |      |      |      |      |      |      |
  *                               `-----------------------------------------'      `-----------------------------------------'
 */ 
@@ -71,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               KC_TRNS,KC_NO,  KC_7   ,  KC_8  ,   KC_9 ,   KC_CAPS,                                        KC_F1  ,   KC_F2,  KC_F3,   KC_F4,  KC_F5,   KC_F6,  \
      KC_TRNS, KC_TRNS, KC_0  ,  KC_4   ,  KC_5  ,   KC_6 ,   KC_INS ,                                        KC_LEFT, KC_DOWN,  KC_UP,KC_RIGHT, KC_F12, KC_TRNS, KC_NO, \
               KC_TRNS,KC_NO,  KC_1   ,  KC_2  ,   KC_3 ,   KC_NO,                                        KC_F7,   KC_F8,    KC_F9,   KC_F10, KC_F11,KC_TRNS,        \
-                                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS \
+                                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, MAC \
     ),
 
 
@@ -85,15 +106,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *`-------+--------+------+------+------+------+------|                                                |------+------+------+------+------+--------+-------'
  *        |KC_TRNS_| XXXX |  END | PGDN | XXXX | XXXX |                                                |   _  |   +  |   [  |   ]  |   \  |KC_TRNS_|
  *        `----------------------+------+------+------+--|-----------------.      .-----------------|--+------+------+------+----------------------'
- *                               |______|______|______|______|______|______|      |______|______|______|______|______|______|
+ *                               |______|______|______|______|______|______|      |______|______|______|______|______|QWERTY|
  *                               |      |      |      |      |      |      |      |      |      |      |      |      |      |
  *                               `-----------------------------------------'      `-----------------------------------------'
  */
     [_SYM] = LAYOUT(\
-              KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,   KC_PERC, KC_CIRC,                                        KC_AMPR,  KC_ASTR, KC_LPRN,  KC_RPRN, KC_UNDS, KC_PLUS, \
+              KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,   KC_PERC, KC_CIRC,                                        KC_AMPR,  KC_ASTR, KC_LPRN,  KC_RPRN, KC_GRV, S(KC_GRV), \
      KC_TRNS, KC_TRNS,KC_NO,   KC_HOME, KC_PGUP, KC_DEL,   KC_PSCR,                                        KC_MINS, KC_EQL,   KC_LCBR, KC_RCBR,  KC_PIPE, KC_TILD, KC_NO, \
               KC_TRNS,KC_NO,   KC_END , KC_PGDN, KC_NO,  KC_NO,                                        KC_UNDS, KC_PLUS,  KC_LBRC, KC_RBRC,  KC_BSLS, KC_TRNS, \
-                                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS \
+                                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QWERTY \
     ),
 
 /*
@@ -134,31 +155,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC, KC_5,  KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,  KC_6,                                             KC_LEFT, KC_DOWN,  KC_UP,KC_RIGHT, KC_F12, KC_TRNS, KC_NO, \
               KC_J,    KC_TRNS,  KC_7, KC_8,  KC_9,  KC_0,                                                KC_F7,   KC_F8,    KC_F9,   KC_F10, KC_F11,KC_TRNS, \
                                 KC_TRNS, KC_L , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, RESET  \
-    )
+    ),
+
+
 
 };
-
 
 #ifdef ENCODER_ENABLE
 bool encoder_update_user(uint8_t index, bool clockwise) {
 
     if (index == 0) {
-        // Volume control
+        // switching windows
         if (clockwise) {
             if (!is_alt_tab_active) {
                is_alt_tab_active = true;
-               register_code(KC_LALT);
+               if (biton32(default_layer_state) == _MAC_QWERTY) {
+                   register_code(KC_RGUI);
+               } else {
+                   register_code(KC_LALT);
+               }
             }
             alt_tab_timer = timer_read();
             tap_code16(KC_TAB);
+        } else {
+        if (!is_alt_tab_active) {
+            is_alt_tab_active = true;
+            if (biton32(default_layer_state) == _MAC_QWERTY) {
+                register_code(KC_RGUI);
             } else {
-            if (!is_alt_tab_active) {
-               is_alt_tab_active = true;
-               register_code(KC_LALT);
+                register_code(KC_LALT);
             }
-            alt_tab_timer = timer_read();
-            tap_code16(S(KC_TAB));
-            }
+        }
+        alt_tab_timer = timer_read();
+        tap_code16(S(KC_TAB));
+        }
     } else if (index == 1) {
         // Page up/Page down
         if (!clockwise) {
@@ -175,8 +205,12 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 void matrix_scan_user(void) {
   if (is_alt_tab_active) {
     if (timer_elapsed(alt_tab_timer) > 750) {
-      unregister_code(KC_LALT);
-      is_alt_tab_active = false;
+        if (biton32(default_layer_state) == _MAC_QWERTY) {
+            unregister_code(KC_RGUI);
+        } else {
+            unregister_code(KC_LALT);
+        }
+        is_alt_tab_active = false;
     }
   }
 }
